6b735e2c44f0fbc6ac9c1ea6c38b25051d10d07b
grhu <mic.grochowski@gmail.com>
2018-01-20 15:09:34 +0100

    v2.2
    Changed paths in manifest and serviceworker

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
 (function(){
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/serviceworker.js').then(function(registration) {
+        navigator.serviceWorker.register('./serviceworker.js').then(function(registration) {
           console.log('ServiceWorker registration successful!');
         }).catch(function(err) {
           console.log('ServiceWorker registration failed: ', err);
